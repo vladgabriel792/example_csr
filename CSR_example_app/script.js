d43c56ff7ad9b04cc95412bc81de217fdff76e07
tupac <82821093+vladgabriel792@users.noreply.github.com>
2023-05-11 00:23:57 +0300

    Application upload #commit1

---
+++ CSR_example_app/script.js
@@ -0,0 +1,50 @@
+const data = [
+  {
+    id: 1,
+    title: 'Big title 1',
+    description: 'Section 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend libero a lorem. Donec molestie urna nisi',
+    link: '/link-1',
+    imageSource: 'https://randompicturegenerator.com/img/car-generator/g23fdf7279b219dc0432e07ad28f06635aaff9fb3d471e20d6b936bd5865f88f68a383fd8baac02e9d586a59b5af6c07b_640.jpg',
+    imageDescription: 'Section 1: Donec vel lacus nibh. Mauris ultrices sem purus.',
+    downloadLinks: [
+      {'Download 1': 'download link'},
+      {'Download 2': 'download link'},
+      {'Download 3': 'download link'},
+      {'Download 4': 'download link'}
+    ],
+    secondaryTitle: 'Small title 1',
+    secondaryDescription: 'Section 1: Proin lacus lacus, luctus sed quam quis'
+  },
+  {
+    id: 2,
+    title: 'Big title 2',
+    description: 'Section 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend libero a lorem. Donec molestie urna nisi',
+    link: '/link-2',
+    imageSource: 'https://randompicturegenerator.com/img/car-generator/g30ca8483cddcc8536f093ca5a80e4e17d7e49eab202dd29bfecab6fb6bc37e92e1b27cd4f6ea693ba94a2de8fc455426_640.jpg',
+    imageDescription: 'Section 2: Donec vel lacus nibh. Mauris ultrices sem purus.',
+    downloadLinks: [
+      {'Download 1': 'download link'},
+      {'Download 2': 'download link'},
+      {'Download 3': 'download link'},
+      {'Download 4': 'download link'}
+    ],
+    secondaryTitle: 'Small title 2',
+    secondaryDescription: 'Section 2: Proin lacus lacus, luctus sed quam quis'
+  },
+  {
+    id: 3,
+    title: 'Big title 3',
+    description: 'Section 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend libero a lorem. Donec molestie urna nisi',
+    link: '/link-3',
+    imageSource: 'https://randompicturegenerator.com/img/car-generator/g9616fc8cd93b5012465c80487bca207748ff921a9a499a227fd3daf03b47e558b1ed2c44aa0de649f489e73485010229_640.jpg',
+    imageDescription: 'Section 3: Donec vel lacus nibh. Mauris ultrices sem purus.',
+    downloadLinks: [
+      {'Download 1': 'download link'},
+      {'Download 2': 'download link'},
+      {'Download 3': 'download link'},
+      {'Download 4': 'download link'}
+    ],
+    secondaryTitle: 'Small title 3',
+    secondaryDescription: 'Section 3: Proin lacus lacus, luctus sed quam quis'
+  }
+];
